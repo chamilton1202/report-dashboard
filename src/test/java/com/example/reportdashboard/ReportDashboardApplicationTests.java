3947f939ee2b19ecd3dff8ffb450df0491b34cf0
Chad J. Hamilton <Chad.Hamilton@nttdata.com>
2020-03-10 06:56:47 -0400

    Initial Version

---
+++ src/test/java/com/example/reportdashboard/ReportDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reportdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
